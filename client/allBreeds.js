c7fad8417b3fe1832f37dfaf28a28920e11672e4
oiibar <95119839+oiibar@users.noreply.github.com>
2022-12-20 19:47:57 +0600

    Update allBreeds.js

--- client/allBreeds.js
+++ client/allBreeds.js
@@ -1,37 +1,29 @@
 const getAllBreeds = () => {
-  let allBreeds;
-  fetch("http:localhost:5000/api/breeds/")
+  fetch("http://localhost:5000/api/breeds/")
     .then((res) => res.json())
     .then((data) => {
-      allBreeds = data;
+      createOptions(data);
+			return data;
     })
-    .then(() => {
-      console.log(allBreeds);
-    })
-    .then(() => {
-      createOptions(allBreeds);
-    });
 };
 getAllBreeds();
 
 const getOneBreed = (id) => {
-  let oneBreed;
-  fetch(`http:localhost:5000/api/breeds/${id}`)
+  fetch(`http://localhost:5000/api/breeds/${id}`)
     .then((res) => res.json())
-    .then((data) => {
-      oneBreed = data
-    })
-    .then(() => {
-      return oneBreed;
-    });
+    .then((data) => data)
 };
 
-const createOptions = (allBreeds) => {
+const createOptions = async (allBreeds) => {
+	let select = document.getElementById('breed_selector');
   for (let i = 0; i < allBreeds.length; i++) {
-    let breed = allBreeds[i];
     let option = document.createElement("option");
-    option.value = i;
-    option.innerHTML = `${breed}`;
-    document.getElementById("breed_selector").appendChild(option);
+		option.innerHTML = `${allBreeds[i].name}`;
+    option.value = allBreeds[i].id;
+		select.appendChild(option);
   }
+	select.onchange = () => {
+		const selectedElement = select.value;
+		console.log(getOneBreed(selectedElement))
+	}
 };
